8950d412859ad26e6105bfb0f9219da3fafb39bd
Matias Suarez Alexander Teran <masuarez18@espe.edu.ec>
2025-03-09 19:08:02 -0500

    V2.0

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-/* app.js */
 function fetchGrades() {
-    fetch('https://conjuntaweb.onrender.com/calculateGrade')
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => {
-                    throw new Error(`Error ${response.status}: ${err.message || response.statusText}`);
-                });
-            }
-            return response.json();
-        })
-        .then(data => {
-            const tableBody = document.querySelector('#gradesTable tbody');
-            tableBody.innerHTML = '';
-            data.forEach(student => {
-                const row = `
-                    <tr>
-                        <td>${student.id}</td>
-                        <td>${student.first_name}</td>
-                        <td>${student.last_name}</td>
-                        <td>${student.subject}</td>
-                        <td>${student.grade1}</td>
-                        <td>${student.grade2}</td>
-                        <td>${student.finalGrade}</td>
-                    </tr>`;
-                tableBody.innerHTML += row;
+    fetch('https://conjuntaweb.onrender.com/calculateGrade', {
+        headers: {
+            'Authorization': 'Bearer rnd_je978oHNmpbdYSYd5Hq3PwtYG2oV'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(err => {
+                throw new Error(`Error ${response.status}: ${err.message || response.statusText}`);
             });
-        })
-        .catch(error => {
-            console.error('Error detallado:', error);
-            alert(`Hubo un problema al cargar las notas: ${error.message}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tableBody = document.querySelector('#gradesTable tbody');
+        tableBody.innerHTML = '';
+        data.forEach(student => {
+            const row = `
+                <tr>
+                    <td>${student.id}</td>
+                    <td>${student.first_name}</td>
+                    <td>${student.last_name}</td>
+                    <td>${student.subject}</td>
+                    <td>${student.grade1}</td>
+                    <td>${student.grade2}</td>
+                    <td>${student.finalGrade}</td>
+                </tr>`;
+            tableBody.innerHTML += row;
         });
+    })
+    .catch(error => {
+        console.error('Error detallado:', error);
+        alert(`Hubo un problema al cargar las notas: ${error.message}`);
+    });
 }
